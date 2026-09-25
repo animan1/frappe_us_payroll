5e1c30d89add3e72259e7bbfb9c87a1180eef627
Daniel Boeve <djboeve@gmail.com>
2026-09-25 18:38:35 +0100

    Run Frappe tests on an isolated site (#24)
    * Run Frappe tests on an isolated site
    
    * Add isolated test site reset target

--- frappe_us_payroll/hooks.py
+++ frappe_us_payroll/hooks.py
@@ -10,6 +10,7 @@ required_apps = ["frappe/erpnext", "frappe/hrms"]
 after_install = "frappe_us_payroll.setup.install_custom_fields"
 after_migrate = "frappe_us_payroll.setup.install_custom_fields"
 before_uninstall = "frappe_us_payroll.setup.uninstall_custom_fields"
+before_tests = "hrms.tests.test_utils.before_tests"
 
 regional_overrides = {
 	"United States": {
